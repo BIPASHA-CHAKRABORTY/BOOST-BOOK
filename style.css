e5743e36d64fa0881221530393d6e7a53a8ac9a4
Bipasha Chakraborty <86914435+BIPASHA-CHAKRABORTY@users.noreply.github.com>
2024-09-30 20:09:17 +0530

    Create style.css

---
+++ style.css
@@ -0,0 +1,74 @@
+body {
+    font-family: Arial, sans-serif;
+    background-color: #0E2344; /* Fallback color */
+    background-image: url('https://media.gettyimages.com/id/903417402/photo/luxury-construction-hotel-with-swimming-pool-at-sunset.jpg?s=612x612&w=0&k=20&c=NyPC_c-wE3W_CImA4t57FpyGy6f428CYROd80jxVC4A=')
+}
+
+.container {
+    max-width: 500px;
+    margin: 50px auto;
+    background-color: rgba(164, 187, 222, 0.8); /* Slightly transparent */
+    padding: 20px;
+    border-radius: 10px;
+    box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+    text-align: center;
+}
+
+
+.container {
+    max-width: 500px;
+    margin: 50px auto;
+    
+    padding: 20px;
+    border-radius: 10px;
+    box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+    text-align: center;
+}
+
+h1 {
+    color: #092248;
+}
+
+p {
+    font-size: 18px;
+    color: #092248;
+}
+
+form {
+    margin-top: 20px;
+}
+
+label {
+    font-size: 16px;
+    display: block;
+    margin-bottom: 5px;
+}
+
+input[type="text"], input[type="number"] {
+    width: 100%;
+    padding: 10px;
+    margin-bottom: 20px;
+    border: 1px solid #092248;
+    border-radius: 5px;
+}
+
+button {
+    background-color: #0E2344;
+    color: #fff;
+    padding: 10px 20px;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+    font-size: 16px;
+}
+
+button:hover {
+    background-color: #D6E0EE;
+}
+
+
+footer {
+    text-align: center;       
+     bottom: 0;
+   
+}
